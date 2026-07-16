218a7b024b115b628a94a62276380ea83b8d8c2f
lithiumagic <mgcusr@gmail.com>
2026-07-15 22:27:11 -0700

    custom handlers

--- main.go
+++ main.go
@@ -5,10 +5,25 @@ import (
 )
 
 func main() {
+	// Create mux that routes URLs to the correct handler.
 	mux := http.NewServeMux()
+
+	// Create a web server that runs on port 8080.
 	newServer := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	// Serve files from the current folder for every URL.
+	// mux.Handle("/app/", http.FileServer(http.Dir(".")))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+
+	})
+
+	// Start the server and wait for requests.
 	newServer.ListenAndServe()
+
 }
