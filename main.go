de56759358ed90b6b69c15ec359847beeb729af1
lithiumagic <mgcusr@gmail.com>
2026-07-20 00:08:57 -0700

    architecture

--- main.go
+++ main.go
@@ -1,27 +1,55 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// something extra here, like logging or counting
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r) // then let the original handler take over
+	})
+}
+
+func (cfg *apiConfig) howManyHits(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) resetHits(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+}
 
 func main() {
 	// Create mux that routes URLs to the correct handler.
 	mux := http.NewServeMux()
+	apiCfg := apiConfig{}
 
 	// Create a web server that runs on port 8080.
 	newServer := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+
+	// mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+
 	// Serve files from the current folder for every URL.
-	// mux.Handle("/app/", http.FileServer(http.Dir(".")))
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 
 	})
+
+	mux.HandleFunc("GET /api/metrics", apiCfg.howManyHits)
+	mux.HandleFunc("POST /api/reset", apiCfg.resetHits)
 
 	// Start the server and wait for requests.
 	newServer.ListenAndServe()
