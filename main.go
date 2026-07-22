699cb1a7b6864ea26c4faed82f15ac18407e8ff4
lithiumagic <mgcusr@gmail.com>
2026-07-21 20:27:37 -0700

    validate

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"sync/atomic"
 )
@@ -19,11 +21,64 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) howManyHits(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())))
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Write([]byte(fmt.Sprintf(`
+	<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>
+	`, cfg.fileserverHits.Load())))
 }
 
 func (cfg *apiConfig) resetHits(w http.ResponseWriter, r *http.Request) {
 	cfg.fileserverHits.Store(0)
+}
+
+func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+
+	type readBody struct {
+		Body string `json:"body"`
+	}
+
+	type validateChirp struct {
+		Valid bool `json:"valid"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := readBody{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(500)
+		w.Write([]byte("{\"error\":\"Error decoding parameters\"}"))
+
+		return
+	}
+	if len(params.Body) > 140 {
+		log.Printf("Error decoding parameters: %s", err)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(400)
+		w.Write([]byte("{\"error\":\"Chirp is too long\"}"))
+		return
+	} else {
+		response := validateChirp{
+			Valid: true,
+		}
+		dat, err := json.Marshal(response)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+			w.WriteHeader(500)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		w.Write(dat)
+
+	}
+
 }
 
 func main() {
@@ -48,8 +103,9 @@ func main() {
 
 	})
 
-	mux.HandleFunc("GET /api/metrics", apiCfg.howManyHits)
-	mux.HandleFunc("POST /api/reset", apiCfg.resetHits)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.howManyHits)
+	mux.HandleFunc("POST /admin/reset", apiCfg.resetHits)
+	mux.HandleFunc("POST /api/validate_chirp", handlerChirpsValidate)
 
 	// Start the server and wait for requests.
 	newServer.ListenAndServe()
